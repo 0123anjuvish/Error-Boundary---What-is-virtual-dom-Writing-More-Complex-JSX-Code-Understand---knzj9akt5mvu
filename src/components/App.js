d9cc84d7ae2ec0b3b69334db2b43e39a603ed16f
Anju Vishwakarma <111855561+0123anjuvish@users.noreply.github.com>
2022-12-14 12:55:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../styles/App.css";
+import Component from "./Component";
+import ErrorBoundary from "./ErrorBoundary";
+const App = () => {
+  let throwErrorFunction;
+  let errorGenerator = () => {
+    throwErrorFunction(true);
+  };
 
-const Navbar = () => {
+  let changeFunc = (updateError) => {
+    throwErrorFunction = updateError;
+  };
   return (
-    <div className="Navbar">
-      <div className="logo">Todo List</div>
-      <button className="theme">Theme</button>
+    <div id="main">
+      <h2>React Error Boundaries</h2>
+      <ErrorBoundary>
+        <Component change={changeFunc} />
+      </ErrorBoundary>
+      <button id="gen" onClick={errorGenerator}>
+        Generate Error
+      </button>
     </div>
   );
 };
 
-export default Navbar;
+export default App;
