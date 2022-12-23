8cb989969f2ae0e8239cd2c8792a3bfb22374d39
Anju Vishwakarma <111855561+0123anjuvish@users.noreply.github.com>
2022-12-23 17:27:36 +0530

    Update ErrorBoundary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ hasError : false };
+  }
+
+  static getDerivedStateFromError(error){
+    return { hasError : true };
+  }
+
+  componentDidCatch(error,errorInfo){
+    logErrorToMyService(error,errorInfo);
+  }
  
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          
+          There is an error in your react app. 
+          Please Check again. This message has been genrated from error boundary.
         </p>
       );
     }
